75e3af4b4286d06bc0b173c257c84bb74c54e224
johnrefani <jhnrefani@gmail.com>
2025-04-22 16:08:19 +0800

    Added the design of student form

---
+++ SchoolInformationSystem/Student/student_frame.vb
@@ -0,0 +1,62 @@
+﻿Public Class student_frame
+    Private Sub studentframe_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        HideAllPanels()
+        dashboardpanel.Visible = True
+        HighlightActiveButton(dashboardbutton)
+    End Sub
+
+    ' Hide all panels
+    Private Sub HideAllPanels()
+        dashboardpanel.Visible = False
+        classespanel.Visible = False
+        myprofilepanel.Visible = False
+    End Sub
+
+    Sub HighlightActiveButton(activeBtn As Button)
+        Dim buttons As Button() = {dashboardbutton, classesbutton, myprofilebutton}
+
+        For Each btn As Button In buttons
+            btn.BackColor = Color.Transparent
+        Next
+
+        activeBtn.BackColor = ColorTranslator.FromHtml("#ADADAD")
+    End Sub
+
+    Private Sub dashboardbutton_Click(sender As Object, e As EventArgs) Handles dashboardbutton.Click
+        HideAllPanels()
+        dashboardpanel.Visible = True
+        HighlightActiveButton(dashboardbutton)
+    End Sub
+
+    Private Sub classesbutton_Click(sender As Object, e As EventArgs) Handles classesbutton.Click
+        HideAllPanels()
+        classespanel.Visible = True
+        HighlightActiveButton(classesbutton)
+    End Sub
+
+    Private Sub myprofilebutton_Click(sender As Object, e As EventArgs) Handles myprofilebutton.Click
+        HideAllPanels()
+        myprofilepanel.Visible = True
+        HighlightActiveButton(myprofilebutton)
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub myprofilepanel_Paint(sender As Object, e As PaintEventArgs) Handles myprofilepanel.Paint
+
+    End Sub
+
+    Private Sub Panel3_Paint(sender As Object, e As PaintEventArgs) Handles Panel3.Paint
+
+    End Sub
+
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs) Handles Panel2.Paint
+
+    End Sub
+End Class
