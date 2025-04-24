c2b44523b4dbfb43bc11271405e8cbfe33b0ee0a
Aljonard Dela Cruz <aljonarddc@gmail.com>
2025-04-24 14:45:34 +0800

    fix application designer file

--- SchoolInformationSystem/obj/Debug/net8.0-windows10.0.17763.0/SchoolInformationSystem.AssemblyInfo.vb
+++ SchoolInformationSystem/obj/Debug/net8.0-windows10.0.17763.0/SchoolInformationSystem.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("SchoolInformationSystem"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b89eab163196e6fef852212ff2699b3a11ad14d"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d391280c3e19268341cc66996267ef04a26d5366"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("SchoolInformationSystem"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("SchoolInformationSystem"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
